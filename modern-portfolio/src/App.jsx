2ec15503ca5d7d21a211a383e2deca9b277d0f54
mwasobaddy <kelvinramsiel@gmail.com>
2025-05-02 17:17:03 +0300

    Complete modern React portfolio with CSS fixes and scroll position restoration

--- modern-portfolio/src/App.jsx
+++ modern-portfolio/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Home from './pages/Home';
@@ -7,9 +8,21 @@ import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Services from './pages/Services';
 
+// ScrollToTop component to reset scroll position on navigation
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow">
